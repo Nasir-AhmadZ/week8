7aea38931f3d3d66c38b4f639ee669b2cb2f4373
Nasir Ahmad <G00419630@atu.ie>
2023-11-21 13:21:03 +0000

    Copied code from the document and created a new file named File13.txt

---
+++ src/main/java/ie/atu/CreateFile.java
@@ -0,0 +1,29 @@
+package ie.atu;
+import java.io.File;
+import java.io.IOException;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class CreateFile {
+    public static void main(String[] args) {
+        //Specify the file name
+        String fileName = "File13.txt";
+
+        // Create a file object
+        File myFile = new File(fileName);
+
+        try {
+            // Create a new file on the disk
+            if (myFile.createNewFile()) {
+                System.out.println("File created successefully at " + myFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists at " + myFile.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file");
+            e.printStackTrace();
+        }
+
+    }
+}
